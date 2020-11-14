745b85be2be3fe9d14378426fed92fa9aaa01b59
kimnamlhn <kimnam.cpc@gmail.com>
2020-11-14 23:12:18 +0700

    add file source and report

---
+++ SimpleShell.cpp
@@ -0,0 +1,261 @@
+#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <io.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#define MAXLINE 80
+void init_reset(char* args[])
+{
+	int dem = 0;
+	for (dem; dem < MAXLINE / 2 + 1; dem++)
+	{
+		args[dem] = NULL;
+	}
+}
+void report_and_exit(const char* msg)
+{
+	perror(msg);
+	exit(-1);
+}
+int KiemTraKiTu_ampersand(char* args[])
+{
+	int i = 0;
+	while (args[i] != 0)
+	{
+		if (strcmp(args[i], "&") == 0)
+		{
+			args[i] = NULL;
+			return 0;
+		}
+		i++;
+	}
+	return 1;
+}
+int KiemTraSuDungPipe(char* args[])
+{
+	int i = 0;
+	while (args[i] != NULL)
+	{
+		if (strcmp(args[i], "|") == 0)
+			return 0;
+		i++;
+	}
+	return 1;
+}
+void ThucThi(char* args[], int check)
+{
+	int temp = fork();
+	if (temp == 0)
+	{
+		execvp(args[0], args);
+		init_reset(args);
+	}
+	else
+	{
+		if (check != 0)
+		{
+			wait(NULL);
+		}
+		else
+		{
+			_exit(0);
+		}
+	}
+}
+int KiemTra_Operator(char* tempt, char* args[])
+{
+	int op = 3;
+	int i = 0;
+	while (args[i] != NULL)
+	{
+		if (strcmp(args[i], "<") == 0)
+		{
+			op = 1;
+			break;
+		}
+		else if (strcmp(args[i], ">") == 0)
+		{
+			op = 2;
+			break;
+		}
+		else
+		{
+			op = 0;
+		}
+		i++;
+	}
+	return op;
+}
+void ThucThiLenhCoChuyenHuong(char* args[], int op, int check)
+{
+	pid_t pid;
+	pid = fork();
+	if (pid == 0)
+	{
+		int i = 0;
+		char file_name[70];
+		while (args[i] != NULL)
+		{
+			if (strcmp(args[i], ">") == 0 || strcmp(args[i], "<") == 0)
+			{
+				args[i] = NULL;
+				strcpy(file_name, args[i + 1]);
+				break;
+			}
+			i++;
+		}
+		int fd;
+		if (op == 1)
+		{
+			fd = open(file_name, O_RDONLY, 0);
+			dup2(fd, STDIN_FILENO);
+		}
+		else
+		{
+			fd = creat(file_name, 0644);
+			dup2(fd, STDOUT_FILENO);
+		}
+		close(fd);
+		execvp(args[0], args);
+		init_reset(args);
+	}
+	else
+	{
+		if (check != 0)
+		{
+			wait(NULL);
+		}
+		else
+		{
+			_exit(0);
+		}
+	}
+}
+void ThucThiLenhCoSuDungPipe(char* args[])
+{
+	char* args1[40];
+	char* args2[40];
+	int pipeFDs[2];
+	int k = 0;
+	for (k; k < 40; k++)
+	{
+		args1[k] = NULL;
+		args2[k] = NULL;
+	}
+	int i = 0;
+	int j = 0;
+	while (strcmp(args[i], "|") != 0 && args[i] != NULL)
+	{
+		args1[i] = (char*)malloc(30);
+		strcpy(args1[i], args[i]);
+		i++;
+	}
+
+	i++;
+	while (args[i] != NULL)
+	{
+		args2[j] = (char*)malloc(30);
+		strcpy(args2[j], args[i]);
+		i++;
+		j++;
+	}
+	if (pipe(pipeFDs) < 0) report_and_exit("pipeFD");
+	int cpid = fork();
+	if (cpid < 0) report_and_exit("fork");
+
+	if (0 == cpid)
+	{    /*** child ***/
+		close(pipeFDs[0]);
+		dup2(pipeFDs[1], STDOUT_FILENO);
+		execvp(args1[0], args1);
+		//perror("execvp");
+		close(pipeFDs[1]);
+		_exit(0);
+	}
+	else
+	{
+
+		close(pipeFDs[1]);
+		dup2(pipeFDs[0], STDIN_FILENO);
+		execvp(args2[0], args2);
+		// perror("execvp");
+		close(pipeFDs[0]);
+		_exit(0);
+	}
+
+}
+void Nhap_XuLy(char* args[], char* History)
+{
+	char* Tempt = (char*)malloc(MAXLINE * sizeof(char));
+	gets(Tempt);
+	if (strcmp(Tempt, "!!") != 0)
+	{
+		strcpy(History, Tempt);
+		int i = 0;
+		args[0] = strtok(Tempt, " ");
+		do
+		{
+			i++;
+			args[i] = strtok(NULL, " ");
+		} while (args[i] != NULL);
+		int check = KiemTraKiTu_ampersand(args);
+		if (KiemTra_Operator(Tempt, args) == 1 || KiemTra_Operator(Tempt, args) == 2)
+		{
+			ThucThiLenhCoChuyenHuong(args, KiemTra_Operator(Tempt, args), check);
+		}
+		else
+		{
+			if (KiemTraSuDungPipe(args) == 0)
+			{
+				int pid2 = fork();
+				if (pid2 == 0) {
+					if (check == 0) printf("[%d]\n", getpid());
+					ThucThiLenhCoSuDungPipe(args);
+					if (check == 0) {
+
+						_exit(0);
+					}
+				}
+				else if (pid2 != 0 && check != 0)
+				{
+					wait(NULL);
+				}
+			}
+			else
+			{
+				ThucThi(args, check);
+			}
+		}
+	}
+	else if (strcmp(Tempt, "!!") == 0 && strcmp(History, "") == 0)
+	{
+		printf("No commands in history\n");
+	}
+	else
+	{
+		printf("previous command: %s\n", History);
+	}
+
+}
+
+int main()
+{
+	char* args[MAXLINE / 2 + 1];
+	init_reset(args);
+	char* History = (char*)malloc(MAXLINE * sizeof(char));
+	strcpy(History, "");
+	int shouldrun = 1;
+	while (shouldrun)
+	{
+		printf("osh>");
+		fflush(stdout);
+		Nhap_XuLy(args, History);
+
+	}
+	return 0;
+}
